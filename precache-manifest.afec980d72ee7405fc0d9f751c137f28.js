b46b23f2b06ce9e34601f06aefc185408f3632c8
cheman121998 <chethiminhman121998@gmail.com>
2020-10-08 15:45:01 +0700

    Updates

---
+++ precache-manifest.afec980d72ee7405fc0d9f751c137f28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4371e300224ee6f2f0d43487e645ea2a",
+    "url": "/lucky/index.html"
+  },
+  {
+    "revision": "3297c71c4767f4d8f01f",
+    "url": "/lucky/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/lucky/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lucky/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3297c71c4767f4d8f01f",
+    "url": "/lucky/static/js/main.3a1a379d.chunk.js"
+  },
+  {
+    "revision": "96f37568e615a903af2b",
+    "url": "/lucky/static/js/runtime-main.ff9b356a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lucky/static/media/logo.5d5d9eef.svg"
+  }
+]);
