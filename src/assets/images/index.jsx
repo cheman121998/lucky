817219611ed4aeeba36920a337dadadc4621c80b
cheman121998 <chethiminhman121998@gmail.com>
2020-10-21 10:20:26 +0700

    Update code to demo

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -1,11 +1,11 @@
-import image1 from './image 1.jpg';
-import image2 from './image 2.jpg';
-import image3 from './image 3.jpg';
-import image4 from './image 4.jpg';
-import image5 from './image 5.jpg';
-import image6 from './image 6.jpg';
-import image7 from './image 7.jpg';
-import avt from './avt.jpg';
+import image1 from './image 1.svg';
+import image2 from './image 2.svg';
+import image3 from './image 3.svg';
+import image4 from './image 4.svg';
+import image5 from './image 5.svg';
+import image6 from './image 6.svg';
+import image7 from './image 7.svg';
+import avt from './avt.svg';
 import emptyPlayer from './Family.png';
 
 export { emptyPlayer, image1, image2, image3, image4, image5, image6, image7, avt };
